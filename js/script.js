d8d7e9add68f7debae3eed70c1507a01b0a9f846
jspark721 <jspark721@gmail.com>
2025-01-13 23:31:32 -0600

    update script

--- js/script.js
+++ js/script.js
@@ -24,3 +24,33 @@ playButton.addEventListener("click", () => {
   mobileVideo.play();
   playButton.remove();
 });
+
+//navigation 
+let open = document.querySelector('.menubox');
+let menu = document.querySelector('.nav--open');
+let close = document.querySelector('.nav--close-icon');
+
+window.onload = function() {
+  menu.style.opacity = "0";
+};
+
+open.addEventListener('click', function() {
+  menu.classList.toggle('close');
+  menu.style.opacity = "1";
+});
+
+close.addEventListener('click', function() {
+  menu.classList.toggle('close');
+});
+
+window.addEventListener('scroll', function() {
+  console.log('scrolling');
+  const navbar = document.querySelector('.navbar'); 
+  const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+  if (scrollTop > 100) { 
+    navbar.classList.add('navshadow'); 
+  } else {
+    navbar.classList.remove('navshadow'); 
+  }
+});
